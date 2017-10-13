7fa3f2b2b70e44a8ca6fae91655ad7994e5249dc
mrchan <wildcatrobotics2011@gmail.com>
2017-10-12 21:55:40 -0700

    started the yt stuff

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var EXE = require('./lib/expressEndpoint'),
-    AXE = require('./lib/alexaEndpoint');
+    AXE = require('./lib/alexaEndpoint'),
+    YTA = require('./lib/alexaYoutube');
 
 AXE.installExpress(EXE.app, 'profanity');
+YTA.installExpress(EXE.app, 'youtube');
